4ebbfc638d9165ba745987d9e55bd7a722e92235
Jhiro Faran <jhirof74@gmail.com>
2025-07-09 22:09:51 +0700

    feat: new service to send message producer

---
+++ src/main/java/com/example/microservice/order/message/KafkaProducerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.microservice.order.message;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerServiceImpl implements KafkaProducerService {
+
+    private final String TOPIC = "order-events";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void sendOrderEvent(Object data) throws JsonProcessingException {
+        String valueAsString = objectMapper.writeValueAsString(data);
+        kafkaTemplate.send(TOPIC, valueAsString);
+    }
+
+
+
+}
