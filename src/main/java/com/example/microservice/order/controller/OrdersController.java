c46225c781b21c6adf7b3fa84fd46037f7a1feb8
Jhiro Faran <jhirof74@gmail.com>
2025-07-09 21:32:37 +0700

    feat: config kafka in springboot

--- src/main/java/com/example/microservice/order/controller/OrdersController.java
+++ src/main/java/com/example/microservice/order/controller/OrdersController.java
@@ -5,35 +5,16 @@ import com.example.microservice.order.constant.APIUrl;
 import com.example.microservice.order.constant.ResponseMessage;
 import com.example.microservice.order.service.OrdersService;
 import com.example.microservice.order.utils.CommonResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = APIUrl.ORDERS)
 public class OrdersController {
 
     private final OrdersService orderService;
-
-    public OrdersController(OrdersService orderService) {
-        this.orderService = orderService;
-        System.out.println("OrdersController loaded ");
-    }
-
-    @GetMapping
-    public String getAllOrders() {
-        System.out.println("getAllOrders called");
-        return "ping";
-    }
-
-    @GetMapping("/testing")
-    public ResponseEntity<CommonResponse<String>> testEndpoint() {
-        CommonResponse<String> response = CommonResponse.<String>builder()
-                .statusCode(200)
-                .message(ResponseMessage.SUCCESS_GET_DATA)
-                .data("This is a test endpoint")
-                .build();
-        return ResponseEntity.ok(response);
-    }
 
     @PostMapping
     public ResponseEntity<CommonResponse<?>> createOrder(@RequestBody RequestOrderDTO requestOrderDTO) {
